93025ec636bdc61a6fe3445a4e077524148dffc6
fpatrick <39106404+fpatrick@users.noreply.github.com>
2021-04-08 14:46:09 +0100

    Update langcodes.py
    fix languages order

--- resources/lib/langcodes.py
+++ resources/lib/langcodes.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 #    This script is based on the one of script.xbmc.subtitles
 #    Thanks to their original authors amet, mr_blobby
@@ -6,9 +6,9 @@
 
 
 LANGUAGES      = (
-    
+
     # Full Language name[0]     podnapisi[1]  ISO 639-1[2]   ISO 639-1 Code[3]   Script Setting Language[4]   localized name id number[5]
-    
+
     ("Albanian"                   , "29",       "sq",            "alb",                 "0",                     30201  ),
     ("Arabic"                     , "12",       "ar",            "ara",                 "1",                     30202  ),
     ("Belarusian"                 , "0" ,       "hy",            "arm",                 "2",                     30203  ),
@@ -22,54 +22,39 @@ LANGUAGES      = (
     ("Dutch"                      , "23",       "nl",            "dut",                 "10",                    30211  ),
     ("English"                    , "2",        "en",            "eng",                 "11",                    30212  ),
     ("Estonian"                   , "20",       "et",            "est",                 "12",                    30213  ),
-    ("Finnish"                    , "31",       "fi",            "fin",                 "13",                    30214  ),
-    ("French"                     , "8",        "fr",            "fre",                 "14",                    30215  ),
-    ("German"                     , "5",        "de",            "ger,deu",             "15",                    30216  ),
-    ("Greek"                      , "16",       "el",            "ell",                 "16",                    30217  ),
-    ("Hebrew"                     , "22",       "he",            "heb",                 "17",                    30218  ),
-    ("Hindi"                      , "42",       "hi",            "hin",                 "18",                    30219  ),
-    ("Hungarian"                  , "15",       "hu",            "hun",                 "19",                    30220  ),
-    ("Icelandic"                  , "6",        "is",            "ice",                 "20",                    30221  ),
-    ("Indonesian"                 , "0",        "id",            "ind",                 "21",                    30222  ),
-    ("Italian"                    , "9",        "it",            "ita",                 "22",                    30223  ),
-    ("Japanese"                   , "11",       "ja",            "jpn",                 "23",                    30224  ),
-    ("Korean"                     , "4",        "ko",            "kor",                 "24",                    30225  ),
-    ("Latvian"                    , "21",       "lv",            "lav",                 "25",                    30226  ),
-    ("Lithuanian"                 , "0",        "lt",            "lit",                 "26",                    30227  ),
-    ("Macedonian"                 , "35",       "mk",            "mac",                 "27",                    30228  ),
-    ("Norwegian"                  , "3",        "no",            "nor",                 "28",                    30229  ),
-    ("Persian"                    , "52",       "fa",            "per",                 "29",                    30230  ),
-    ("Polish"                     , "26",       "pl",            "pol",                 "30",                    30231  ),
-    ("Portuguese"                 , "32",       "pt",            "por",                 "31",                    30232  ),
-    ("Portuguese (Brazil)"        , "48",       "pb",            "pt-br",               "32",                    30233  ),
-    ("Romanian"                   , "13",       "ro",            "rum",                 "33",                    30234  ),
-    ("Russian"                    , "27",       "ru",            "rus",                 "34",                    30235  ),
-    ("Serbian"                    , "36",       "sr",            "scc",                 "35",                    30236  ),
-    ("Slovak"                     , "37",       "sk",            "slo",                 "36",                    30237  ),
-    ("Slovenian"                  , "1",        "sl",            "slv",                 "37",                    30238  ),
-    ("Spanish"                    , "28",       "es",            "spa",                 "38",                    30239  ),
-    ("Swedish"                    , "25",       "sv",            "swe",                 "39",                    30240  ),
-    ("Thai"                       , "0",        "th",            "tha",                 "40",                    30241  ),
-    ("Turkish"                    , "30",       "tr",            "tur",                 "41",                    30242  ),
-    ("Ukrainian"                  , "46",       "uk",            "ukr",                 "42",                    30243  ),
-    ("Vietnamese"                 , "51",       "vi",            "vie",                 "43",                    30244  ),
-    ("BosnianLatin"               , "10",       "bs",            "bos",                 "100",                   30245  ),
-    ("Farsi"                      , "52",       "fa",            "per",                 "13",                    30246  ),
-    ("English (US)"               , "2",        "en",            "eng",                 "100",                   30212  ),
-    ("English (UK)"               , "2",        "en",            "eng",                 "100",                   30212  ),
-    ("Portuguese (Brazilian)"     , "48",       "pt-br",         "pob",                 "100",                   30234  ),
-    ("Portuguese (Brazil)"        , "48",       "pb",            "pob",                 "32",                    30234  ),
-    ("Portuguese-BR"              , "48",       "pb",            "pob",                 "32",                    30234  ),
-    ("Brazilian"                  , "48",       "pb",            "pob",                 "32",                    30234  ),
-    ("Español (Latinoamérica)"    , "28",       "es",            "spa",                 "100",                   30240  ),
-    ("Español (España)"           , "28",       "es",            "spa",                 "100",                   30240  ),
-    ("Spanish (Latin America)"    , "28",       "es",            "spa",                 "100",                   30240  ),
-    ("Español"                    , "28",       "es",            "spa",                 "100",                   30240  ),
-    ("SerbianLatin"               , "36",       "sr",            "scc",                 "100",                   30237  ),
-    ("Spanish (Spain)"            , "28",       "es",            "spa",                 "100",                   30240  ),
-    ("Chinese (Traditional)"      , "17",       "zh",            "chi",                 "100",                   30207  ),
-    ("Chinese (Simplified)"       , "17",       "zh",            "chi",                 "100",                   30207  ),
-    ("None"                       , "-1",       "",              "non",                 "44",                    30200  ) )
+    ("Farsi"                      , "52",       "fa",            "per",                 "13",                    30214  ),
+    ("Finnish"                    , "31",       "fi",            "fin",                 "14",                    30215  ),
+    ("French"                     , "8",        "fr",            "fre",                 "15",                    30216  ),
+    ("German"                     , "5",        "de",            "ger,deu",             "16",                    30217  ),
+    ("Greek"                      , "16",       "el",            "ell",                 "17",                    30218  ),
+    ("Hebrew"                     , "22",       "he",            "heb",                 "18",                    30219  ),
+    ("Hindi"                      , "42",       "hi",            "hin",                 "19",                    30220  ),
+    ("Hungarian"                  , "15",       "hu",            "hun",                 "20",                    30221  ),
+    ("Icelandic"                  , "6",        "is",            "ice",                 "21",                    30222  ),
+    ("Indonesian"                 , "0",        "id",            "ind",                 "22",                    30223  ),
+    ("Italian"                    , "9",        "it",            "ita",                 "23",                    30224  ),
+    ("Japanese"                   , "11",       "ja",            "jpn",                 "24",                    30225  ),
+    ("Korean"                     , "4",        "ko",            "kor",                 "25",                    30226  ),
+    ("Latvian"                    , "21",       "lv",            "lav",                 "26",                    30227  ),
+    ("Lithuanian"                 , "0",        "lt",            "lit",                 "27",                    30228  ),
+    ("Macedonian"                 , "35",       "mk",            "mac",                 "28",                    30229  ),
+    ("Norwegian"                  , "3",        "no",            "nor",                 "29",                    30230  ),
+    ("Persian"                    , "52",       "fa",            "per",                 "30",                    30231  ),
+    ("Polish"                     , "26",       "pl",            "pol",                 "31",                    30232  ),
+    ("Portuguese"                 , "32",       "pt",            "por",                 "32",                    30233  ),
+    ("Portuguese (Brazil)"        , "48",       "pb",            "pt-br",               "33",                    30234  ),
+    ("Romanian"                   , "13",       "ro",            "rum",                 "34",                    30235  ),
+    ("Russian"                    , "27",       "ru",            "rus",                 "35",                    30236  ),
+    ("Serbian"                    , "36",       "sr",            "scc",                 "36",                    30237  ),
+    ("Slovak"                     , "37",       "sk",            "slo",                 "37",                    30238  ),
+    ("Slovenian"                  , "1",        "sl",            "slv",                 "38",                    30239  ),
+    ("Spanish"                    , "28",       "es",            "spa",                 "39",                    30240  ),
+    ("Swedish"                    , "25",       "sv",            "swe",                 "40",                    30241  ),
+    ("Thai"                       , "0",        "th",            "tha",                 "41",                    30242  ),
+    ("Turkish"                    , "30",       "tr",            "tur",                 "42",                    30243  ),
+    ("Ukrainian"                  , "46",       "uk",            "ukr",                 "43",                    30244  ),
+    ("Vietnamese"                 , "51",       "vi",            "vie",                 "44",                    30245  ),
+    ("None"                       , "-1",       "",              "non",                 "45",                    30200  ) )
 
 def languageTranslate(lang, lang_from, lang_to):
   for x in LANGUAGES:
