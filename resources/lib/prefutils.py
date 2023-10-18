cddbb1f04fd61e792236607f290bae25e2e441f6
rockrider69 <family.pardon@outlook.fr>
2023-10-18 17:41:26 +0200

    Update prefutils.py

--- resources/lib/prefutils.py
+++ resources/lib/prefutils.py
@@ -55,8 +55,8 @@ class LangPrefMan_Player(xbmc.Player) :
             log(LOG_DEBUG, 'Getting video properties')
             self.getDetails()
             self.evalPrefs()
-            # force immediate rewind to avoid 10-15sec delay and first few subtitles lines potentially lost
-            self.seekTime(0)
+            # force short rewind to avoid 10-15sec delay and first few subtitles lines potentially lost
+            self.seekTime(self.getTime()-1)
             self.LPM_initial_run_done = True
 
     def onAVChange(self):
