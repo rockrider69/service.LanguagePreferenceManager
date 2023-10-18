9084cdd85d071ef35c69583a39e2eb70d152f88b
rockrider69 <family.pardon@outlook.fr>
2023-10-18 22:15:08 +0200

    Update prefutils.py
    Test if sub forced is false or true now includes using the subtitle stream field  isforced in addition to just the name which might be missing 'forced' text

--- resources/lib/prefutils.py
+++ resources/lib/prefutils.py
@@ -317,7 +317,7 @@ class LangPrefMan_Player(xbmc.Player) :
         test = subName.lower()
         matches = ['forced', 'forcés']
         found = any(x in test for x in matches)
-        return ((forced == 'false') and not found) or ((forced == 'true') and found) or ((forced == 'true') and subForcedTag == 'True')
+        return ((forced == 'false') and not found) or ((forced == 'true') and found) or ((forced == 'true') and subForcedTag == 'True') or ((forced == 'false') and subForcedTag == 'False')
 
     def isExternalSub(self, subName):
         test = subName.lower()
