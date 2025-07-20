7544fb299f077b91d80b75e179ead3f850d7157e
rockrider69 <family.pardon@outlook.fr>
2025-07-20 22:20:14 +0200

    Fix circular import around settings

--- resources/lib/logger.py
+++ resources/lib/logger.py
@@ -1,15 +1,15 @@
 import xbmc, xbmcaddon
-from prefsettings import settings
 
 LOG_NONE = 0
 LOG_ERROR = 1
 LOG_INFO = 2
 LOG_DEBUG = 3
 
-settings = settings()
+addon = xbmcaddon.Addon()
+log_level = addon.getSetting('log_level')
+
 
 def log(level, msg):
-    log_level = settings.logLevel
 
     if level <= log_level:
         if level == LOG_ERROR:
